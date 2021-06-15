6cc102847477a22666776487e010e1c67d9328a0
epixinvites <epixunknown@protonmail.com>
2021-06-15 23:08:09 +0800

    Tue Jun 15 11:08:09 PM +08 2021

--- Beginner/348-operator_overloading.cc
+++ Beginner/348-operator_overloading.cc
@@ -1,2 +1,21 @@
 #include "std_lib_facilities.h"
 using namespace std;
+enum class Month{
+    jan=1,
+    feb,
+    mar,
+    apr,
+    may,
+    jun,
+    jul,
+    aug,
+    sep,
+    oct,
+    nov,
+    dec
+};
+
+Month operator++(Month &m){
+    m = (m==Month::dec)?Month::jan:Month(int(m)+1); // 
+    return m;
+}
