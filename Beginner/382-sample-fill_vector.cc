731e5ef5eed977d19ae4071f41662f6e954f249a
epixinvites <epixunknown@protonmail.com>
2021-06-22 20:15:52 +0800

    Tue Jun 22 08:15:52 PM +08 2021

--- Beginner/382-sample-fill_vector.cc
+++ Beginner/382-sample-fill_vector.cc
@@ -8,7 +8,7 @@ void fill_vector(istream &ist, vector<int> &v, char terminator){
         return;
     }
     if(ist.bad()){ // stream corrupted
-        error("Input stream bad");
+        error("Input stream bad"); // ist.exceptions(ist.exceptions()|ios_base::failbit);
     }
     if(ist.fail()){ // clean up the mess and report the problem
         ist.clear(); // lear stream state
@@ -17,5 +17,20 @@ void fill_vector(istream &ist, vector<int> &v, char terminator){
             ist.unget(); // put char back
             ist.clear(ios_base::failbit); // set state to fail()
         }
+    }
+}
+
+void fill_vector_v2(istream &ist, vector<int> &v, char terminator){
+    for(int i; ist>>i;){
+        v.push_back(i);
+    }
+    if(ist.eof()){
+        return;
+    }
+    ist.clear(); // not good(), bad(), eof() then it should be fail(). clear() stream state
+    char c; ist>>c;
+    if(c!=terminator){
+        ist.unget();
+        ist.clear(ios_base::failbit);
     }
 }
