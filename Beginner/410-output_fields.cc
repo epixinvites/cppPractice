506b11efe561a6bcda4e76e1e3f633f6d921023c
epixinvites <epixunknown@protonmail.com>
2021-06-28 13:21:26 +0800

    Mon Jun 28 01:21:26 PM +08 2021

---
+++ Beginner/410-output_fields.cc
@@ -0,0 +1,7 @@
+#include "std_lib_facilities.h"
+using namespace std;
+int main(){
+    cout<<123456789<<endl;
+    cout<<setw(5)<<123456789<<endl;
+    cout<<'|'<<setw(12)<<123456789<<'|'<<endl;
+}
