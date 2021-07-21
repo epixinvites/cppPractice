bf94060d3338b37574114b91e6ddf0c9f30c0074
epixinvites <epixunknown@protonmail.com>
2021-07-21 19:17:38 +0800

    Wed Jul 21 07:17:38 PM +08 2021

--- Beginner/421-using_nonsandard_separators.cc
+++ Beginner/421-using_nonsandard_separators.cc
@@ -12,6 +12,7 @@ public:
     void add_white(char c){white+=c;}
     bool is_whitespace(char c);
     void case_sensitive(bool b){sensitive=b;}
+    bool is_case_sensitive(){return sensitive;}
     Punct_stream &operator>>(string &s);
     operator bool();
 };
