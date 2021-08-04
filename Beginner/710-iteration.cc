dae4b3fbb617d6324c41aea8dfdfb07dc316a2e3
epixinvites <epixunknown@protonmail.com>
2021-08-04 19:00:01 +0800

    Wed Aug  4 07:00:01 PM +08 2021

--- Beginner/710-iteration.cc
+++ Beginner/710-iteration.cc
@@ -39,5 +39,14 @@ std::istream &operator>>(std::istream is, Document &d){
     return is;
 }
 void print(Document &d){
-    for(auto p:d) std::cout<<*p;
+    for(auto p:d) std::cout<<p;
+}
+void erase_line(Document &d, int n){
+    if(n<0||d.line.size()-1<=n) return;
+    auto p = d.line.begin();
+    advance(p,n);
+    d.line.erase(p);
+}
+template<typename Iter> void advance(Iter &p, int n){
+    while(0<n){++p; --n;}
 }
