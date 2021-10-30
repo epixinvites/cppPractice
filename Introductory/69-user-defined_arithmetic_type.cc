2235e9ca488622d63908c116cc38b8094c833a6f
epixinvites <epixunknown@protonmail.com>
2021-10-30 19:00:01 +0800

    Sat Oct 30 07:00:01 PM +08 2021

--- Introductory/69-user-defined_arithmetic_type.cc
+++ Introductory/69-user-defined_arithmetic_type.cc
@@ -26,6 +26,29 @@ public:
     complex &operator/=(complex);
 };
 
+complex operator+(complex a, complex b) { return a += b; }
+complex operator-(complex a, complex b) { return a -= b; }
+complex operator-(complex a) { return {-a.real(), -a.imag()}; }
+complex operator*(complex a, complex b) { return a *= b; }
+complex operator/(complex a, complex b) { return a /= b; }
+
+bool operator==(complex a, complex b){
+    return a.real()==b.real() && a.imag()==b.imag();
+}
+
+bool operator!=(complex a, complex b){
+    return !(a==b);
+}
+
+void f(complex z){
+    complex a {2.3};
+    complex b {1/a};
+    complex c {a+z*complex{1,2.3}};
+    if(c!=b){
+        c=-(b/a)+a*b;
+    }
+}
+
 int main(){
     complex z = {1,0};
     const complex cz {1,3};
