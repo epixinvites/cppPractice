e2cf290cc08cdcd07d68ac76bce03684cf82dcc7
epixinvites <epixunknown@protonmail.com>
2021-11-03 19:00:01 +0800

    Wed Nov  3 07:00:01 PM +08 2021

--- Introductory/109-template_argument_deduction.cc
+++ Introductory/109-template_argument_deduction.cc
@@ -5,6 +5,13 @@ public:
     Vector(std::initializer_list<T>);
 };
 
+template<typename T> class Vector2{
+public:
+    Vector2(std::initializer_list<T>);
+    template<typename Iter> Vector2(Iter b, Iter e); // range constructor
+    template<typename Iter> Vector2(Iter, Iter) -> Vector2<typename Iter::T>;
+};
+
 int main(){
     Vector v1 = {1, 2, 3};
     Vector v2 = {'a', 'b', 'c'};
