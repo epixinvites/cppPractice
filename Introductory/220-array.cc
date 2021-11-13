e01add1e0ff6852dd7942aed3f564a926f6a781c
epixinvites <epixunknown@protonmail.com>
2021-11-13 19:00:01 +0800

    Sat Nov 13 07:00:01 PM +08 2021

--- Introductory/220-array.cc
+++ Introductory/220-array.cc
@@ -1,1 +1,9 @@
+#include <algorithm>
 #include <array>
+void f(int *p, int sz);
+int main(){
+    std::array<int, 3> a1 = {1, 2, 3};
+    f(&a1[0], a1.size());
+
+    auto p = std::find(a1.begin(), a1.end(), 3);
+}
