92db458a838d6fa206d1682b36109dfdc4e69e47
epixinvites <epixunknown@protonmail.com>
2021-12-25 18:18:36 +0800

    Sat Dec 25 06:18:36 PM +08 2021

---
+++ C++_Game_Development_by_Example/SFML_Subproject/main.cc
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+
+int main(){
+	sf::Vector2f viewSize(1024, 768);
+	sf::VideoMode vm(viewSize.x, viewSize.y);
+	sf::RenderWindow window(vm, "Test window", sf::Style::Default);
+
+	while(window.isOpen()){
+		window.clear(sf::Color::Black);
+		window.display();
+	}
+
+	return 0;
+}
