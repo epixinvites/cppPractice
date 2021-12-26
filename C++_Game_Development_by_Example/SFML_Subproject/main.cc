25737f31cb9abfd2291193fef6d20041a75cb8e9
epixinvites <epixunknown@protonmail.com>
2021-12-26 19:00:01 +0800

    Sun Dec 26 07:00:01 PM +08 2021

--- C++_Game_Development_by_Example/SFML_Subproject/main.cc
+++ C++_Game_Development_by_Example/SFML_Subproject/main.cc
@@ -1,12 +1,18 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-	sf::Vector2f viewSize(1024, 768);
-	sf::VideoMode vm(viewSize.x, viewSize.y);
-	sf::RenderWindow window(vm, "Test window", sf::Style::Default);
+	sf::Vector2f viewSize{600, 400};
+	sf::VideoMode vm{static_cast<unsigned int>(viewSize.x), static_cast<unsigned int>(viewSize.y)};
+	sf::RenderWindow window{vm, "Test window", sf::Style::Default};
+
+	sf::RectangleShape rect{sf::Vector2f{static_cast<float>(300), static_cast<float>(150)}};
+	rect.setFillColor(sf::Color::Cyan);
+	rect.setPosition({viewSize.x/2, viewSize.y/2});
+	rect.setOrigin(sf::Vector2f{rect.getSize().x/2, rect.getSize().y/2});
 
 	while(window.isOpen()){
 		window.clear(sf::Color::Black);
+		window.draw(rect);
 		window.display();
 	}
 
