a0e5a4ca4758a816a61fe62ba1cdb92bdcb546b1
epixinvites <epixunknown@protonmail.com>
2021-12-27 19:00:01 +0800

    Mon Dec 27 07:00:01 PM +08 2021

--- C++_Game_Development_by_Example/SFML_Subproject/main.cc
+++ C++_Game_Development_by_Example/SFML_Subproject/main.cc
@@ -1,18 +1,33 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-	sf::Vector2f viewSize{600, 400};
+	sf::Vector2f viewSize{1024, 576};
 	sf::VideoMode vm{static_cast<unsigned int>(viewSize.x), static_cast<unsigned int>(viewSize.y)};
 	sf::RenderWindow window{vm, "Test window", sf::Style::Default};
 
-	sf::RectangleShape rect{sf::Vector2f{static_cast<float>(300), static_cast<float>(150)}};
+	sf::RectangleShape rect{sf::Vector2f{static_cast<float>(500), static_cast<float>(250)}};
 	rect.setFillColor(sf::Color::Cyan);
 	rect.setPosition({viewSize.x/2, viewSize.y/2});
 	rect.setOrigin(sf::Vector2f{rect.getSize().x/2, rect.getSize().y/2});
 
+	sf::CircleShape circle{100};
+	circle.setFillColor(sf::Color::White);
+	circle.setPosition({viewSize.x/2, viewSize.y/2});
+	circle.setOrigin(sf::Vector2f{circle.getRadius(), circle.getRadius()});
+
+	sf::ConvexShape triangle;
+	triangle.setPointCount(3);
+	triangle.setPoint(0, sf::Vector2f{-100, 0});
+	triangle.setPoint(1, sf::Vector2f{0, -100});
+	triangle.setPoint(2, sf::Vector2f{100, 0});
+	triangle.setFillColor(sf::Color{128, 0, 128, 255});
+	triangle.setPosition({viewSize.x/2, viewSize.y/2});
+
 	while(window.isOpen()){
 		window.clear(sf::Color::Black);
 		window.draw(rect);
+		window.draw(circle);
+		window.draw(triangle);
 		window.display();
 	}
 
