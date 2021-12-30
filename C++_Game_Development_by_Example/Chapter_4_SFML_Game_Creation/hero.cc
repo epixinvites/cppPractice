676836541c98880ad62524a6ffcabb68b76aa089
epixinvites <epixunknown@protonmail.com>
2021-12-30 19:00:01 +0800

    Thu Dec 30 07:00:01 PM +08 2021

--- C++_Game_Development_by_Example/Chapter_4_SFML_Game_Creation/hero.cc
+++ C++_Game_Development_by_Example/Chapter_4_SFML_Game_Creation/hero.cc
@@ -11,4 +11,10 @@ void Hero::update(float delta, const sf::Vector2f& viewSize){
 	velocity -= mass*gravity*delta;
 	position.y -= velocity*delta;
 	sprite.setPosition(position);
+	if(position.y>=viewSize.y*0.75){
+		position.y=viewSize.y*0.75;
+		velocity=0;
+		isOnGround=true;
+		jumpCount=0;
+	}
 }
